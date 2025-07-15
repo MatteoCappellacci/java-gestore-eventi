9a229e5023c85196240a6d785e1fa85df9ba8fbd
MatteoCappellacci <Matteo.cappellacci1987@gmail.com>
2025-07-15 18:31:37 +0200

    prova2

--- java-gestore-eventi/src/org/gestore/eventi/Concerto.java
+++ java-gestore-eventi/src/org/gestore/eventi/Concerto.java
@@ -1,5 +1,17 @@
 package org.gestore.eventi;
 
+import java.time.LocalDate;
+
 public class Concerto {
+
+    private String titolo;
+
+    private LocalDate data;
+
+    private int postiTotali;
+
+    private int postiPrenotati;
+
+    
 
 }
